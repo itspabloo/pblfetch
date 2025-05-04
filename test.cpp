09c2925891a2dcb02c3be848ff2dab83b8da882b
itspabloo <hse_govno@baza.zov>
2025-05-04 19:01:48 +0300

    updated test.cpp

--- test.cpp
+++ test.cpp
@@ -3,14 +3,15 @@
 
 int32_t main() {
     SysInfo data;
-    std::cout << data.username() << "@" << data.hostname() << "\n\n";
-    std::cout << "Uptime: " << data.uptime() << '\n';
-    std::cout << "Os: " << data.os() << '\n';
-    std::cout << "Kernel: " << data.kernel() << '\n';
-    std::cout << "Desktop: " << data.desktop() << '\n';
-    std::cout << "Shell: " << data.shell() << '\n';
-    std::cout << "Terminal: " << data.terminal() << '\n';
-    std::cout << "CPU: " << data.cpu() << '\n';
-    std::cout << "Memory: " << data.meminfo() << '\n';
+    std::cout << "\033[95m\033[1m" << data.username() <<
+    "\033[39m\033[1m@\033[95m" << data.hostname() << "\n\n";
+    std::cout << "\033[95m\033[1mUptime: \033[0m" << data.uptime() << '\n';
+    std::cout << "\033[95m\033[1mOs: \033[0m" << data.os() << '\n';
+    std::cout << "\033[95m\033[1mKernel: \033[0m" << data.kernel() << '\n';
+    std::cout << "\033[95m\033[1mDesktop: \033[0m" << data.desktop() << '\n';
+    std::cout << "\033[95m\033[1mShell: \033[0m" << data.shell() << '\n';
+    std::cout << "\033[95m\033[1mTerminal: \033[0m" << data.terminal() << '\n';
+    std::cout << "\033[95m\033[1mCPU: \033[0m" << data.cpu() << '\n';
+    std::cout << "\033[95m\033[1mMemory: \033[0m" << data.meminfo() << '\n';
     return 0;
 }
