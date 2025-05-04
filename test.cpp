7d3278420bd55a841e7b65eb8aceb6cd44638996
itspabloo <hse_govno@baza.zov>
2025-05-04 19:23:47 +0300

    updated test.cpp

--- test.cpp
+++ test.cpp
@@ -3,15 +3,15 @@
 
 int32_t main() {
     SysInfo data;
-    std::cout << "\033[95m\033[1m" << data.username() <<
-    "\033[39m\033[1m@\033[95m" << data.hostname() << "\n\n";
-    std::cout << "\033[95m\033[1mUptime: \033[0m" << data.uptime() << '\n';
-    std::cout << "\033[95m\033[1mOs: \033[0m" << data.os() << '\n';
-    std::cout << "\033[95m\033[1mKernel: \033[0m" << data.kernel() << '\n';
-    std::cout << "\033[95m\033[1mDesktop: \033[0m" << data.desktop() << '\n';
-    std::cout << "\033[95m\033[1mShell: \033[0m" << data.shell() << '\n';
-    std::cout << "\033[95m\033[1mTerminal: \033[0m" << data.terminal() << '\n';
-    std::cout << "\033[95m\033[1mCPU: \033[0m" << data.cpu() << '\n';
-    std::cout << "\033[95m\033[1mMemory: \033[0m" << data.meminfo() << '\n';
+    std::cout << MAIN_COLOR << BOLD << data.username() << DEFAULT << BOLD <<
+    "@" << MAIN_COLOR << data.hostname() << "\n\n";
+    std::cout << "Uptime: " << DEFAULT << data.uptime() << '\n';
+    std::cout << MAIN_COLOR << BOLD << "Os: " << DEFAULT << data.os() << '\n';
+    std::cout << MAIN_COLOR << BOLD << "Kernel: " << DEFAULT << data.kernel() << '\n';
+    std::cout << MAIN_COLOR << BOLD << "Desktop: " << DEFAULT << data.desktop() << '\n';
+    std::cout << MAIN_COLOR << BOLD << "Shell: " << DEFAULT << data.shell() << '\n';
+    std::cout << MAIN_COLOR << BOLD << "Terminal: " << DEFAULT << data.terminal() << '\n';
+    std::cout << MAIN_COLOR << BOLD << "CPU: " << DEFAULT << data.cpu() << '\n';
+    std::cout << MAIN_COLOR << BOLD << "Memory: " << DEFAULT << data.meminfo() << '\n';
     return 0;
 }
